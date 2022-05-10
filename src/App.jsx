b1db88f0a93110cb95b956f33517561b73a23f55
Nicolas Orozco Davila <nico.orozco.davila@yandex.com>
2022-05-09 22:16:00 -0500

    Fix unused custom color

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         [data, loading, error, refetch]
       )}
     >
-      <section className="max-w-lg p-5 m-4 rounded-lg bg-blue-200 font-manrope flex flex-col items-center justify-center flex-1">
+      <section className="max-w-lg p-5 m-4 rounded-lg bg-blue-100 font-manrope flex flex-col items-center justify-center flex-1">
         <Label />
         <Advice />
         <Divider />
